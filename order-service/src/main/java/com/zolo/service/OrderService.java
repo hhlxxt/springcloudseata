0f5c02fca71e29e0a7fad648c63923817bbac30b
hhlxxt <hhlx881101>
2019-10-01 09:30:55 +0800

    修改订单创建方法：返回订单号

--- order-service/src/main/java/com/zolo/service/OrderService.java
+++ order-service/src/main/java/com/zolo/service/OrderService.java
@@ -3,7 +3,15 @@ package com.zolo.service;
 import com.zolo.feign.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 @Service
@@ -15,13 +23,34 @@ public class OrderService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void create(String userId, String commodityCode, Integer count) {
+    public int create(String userId, String commodityCode, Integer count) {
 
         int orderMoney = count * 100;
-        jdbcTemplate.update("insert order_tbl(user_id,commodity_code,count,money) values(?,?,?,?)",
-            new Object[] {userId, commodityCode, count, orderMoney});
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = "insert order_tbl(user_id,commodity_code,count,money,wms_status) values(?,?,?,?,?)" ;
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, userId);
+                ps.setString(2, commodityCode);
+                ps.setInt(3, count);
+                ps.setInt(4, orderMoney);
+                ps.setString(5,"0");
+                return ps;
+            }
+        },keyHolder);
+        /*jdbcTemplate.update("insert order_tbl(user_id,commodity_code,count,money) values(?,?,?,?)",
+            new Object[] {userId, commodityCode, count, orderMoney});*/
 
         userFeignClient.reduce(userId, orderMoney);
 
+        return keyHolder.getKey().intValue();
+
+
+    }
+
+    public int updateOrderStatusById(int orderId, String wms_status) {
+       return jdbcTemplate.update("update order_tbl set wms_status= ? where id = ? ",new Object[]{wms_status,orderId});
     }
 }
